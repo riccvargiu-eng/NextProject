90ef1e29961c64f0e6e1dfaa4430215bcf453ff6
Riccardo V. <ricc.vargiu@gmail.com>
2025-12-11 14:22:44 +0100

    Add global styles and layout for the application
    - Created a new globals.css file to define global styles and color themes.
    - Implemented a RootLayout component in layout.js to wrap the application with HTML structure and global styles.
    - Added a Home component in page.js with a centered layout and responsive design.

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+      <main className="flex flex-col items-center justify-center py-32 px-16"></main>
+    </div>
+  );
+}
